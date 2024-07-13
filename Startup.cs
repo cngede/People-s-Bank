601c03b633df8b6d9ea1488b5e28129db4cc06dd
Cajetan Ngede <134719269+cngede@users.noreply.github.com>
2024-07-13 14:05:47 -0400

    Uploaded startup file

---
+++ Startup.cs
@@ -0,0 +1,79 @@
+﻿using OnlineBank.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace OnlineBank
+{
+    
+
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("OnlineBank")));
+
+
+        services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+        // Adding Authentication 
+        services.AddAuthentication();
+
+
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            // Configure Customize password requirements, lockout settings, etc.
+        });
+
+
+
+        services.AddSession(options =>
+        {
+            options.Cookie.Name = "OnlineBank.Session";
+            options.IdleTimeout = System.TimeSpan.FromMinutes(30);
+            options.Cookie.IsEssential = true;
+        });
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseSession();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Authentication}/{action=Login}/{id?}");
+        });
+    }
+}
+}
